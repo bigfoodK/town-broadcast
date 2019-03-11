2bf6aa5f4a32291a95cb87da1eba3fe49c3b72e3
bigfood <satcom1600@hanmail.net>
2019-03-12 03:00:26 +0900

    Communicate server and client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,132 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+interface State {
+  isOnAir: boolean;
+  isConnected: boolean;
+  isGettingUserMicData: boolean;
+}
+
+class App extends Component<any, State> {
+  socket: WebSocket | null;
+  processor: ScriptProcessorNode | null;
+
+  constructor(prop: any) {
+    super(prop);
+
+    this.socket = null;
+    this.processor = null;
+
+    this.state = {
+      isOnAir: false,
+      isConnected: false,
+      isGettingUserMicData: false,
+    }
+
+    this.makeConnection = this.makeConnection.bind(this);
+    this.getUserMicData = this.getUserMicData.bind(this);
+    this.startBroadcast = this.startBroadcast.bind(this);
+    this.stopBroadcast = this.stopBroadcast.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.makeConnection();
+  }
+  
+  makeConnection() {
+    if(this.socket && this.socket.readyState !== 3) return;
+
+    this.socket = new WebSocket('ws://localhost:3000');
+    this.socket.onerror = error => {
+      console.error(error);
+    }
+    this.socket.onopen = () => {
+      this.setState({
+        isConnected: true,
+      });
+    }
+    this.socket.onclose = () => {
+      this.setState({
+        isConnected: false,
+      });
+      console.log('Disconnected, Try reconnect in 5sec');
+      setTimeout(() => {
+        this.makeConnection();
+      }, 5000);
+    }
+  }
+
+  async getUserMicData() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false
+      });
+
+      const context = new AudioContext();
+      const input = context.createMediaStreamSource(stream)
+      this.processor = context.createScriptProcessor(1024,1,1);
+  
+      input.connect(this.processor);
+      this.processor.connect(context.destination);
+
+      this.setState({
+        isGettingUserMicData: true,
+      })
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+  
+  startBroadcast() {
+    if(!this.processor) return;
+    this.processor.onaudioprocess = e => {
+      if(!this.socket) return;
+      if(this.socket.readyState !== 1) return;
+      const arrayBuffer = convertFloat32ArrayToInt16Array(e.inputBuffer.getChannelData(0));
+      this.socket.send(arrayBuffer);
+    };
+    this.setState({
+      isOnAir: true,
+    });
+  }
+  
+  stopBroadcast() {
+    if(!this.processor) return;
+    this.processor.onaudioprocess = () => {};
+    this.setState({
+      isOnAir: false,
+    });
+  }
+  
+  async handleClick() {
+    if(!this.state.isGettingUserMicData) await this.getUserMicData();
+    this.state.isOnAir
+      ? this.stopBroadcast()
+      : this.startBroadcast()
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>{this.state.isConnected ? 'Connected' : 'Disconnected'} to server</h1>
+      <h1>{this.state.isGettingUserMicData ? 'using' : 'not using'} mic</h1>
+      <h1>{this.state.isOnAir ? 'onAir': 'ready'}</h1>
+        <button 
+          className = 'mic-button'
+          onClick = {this.handleClick}>
+          {this.state.isOnAir ? 'stop broadcast' : 'start broadcast'}
+        </button>
       </div>
     );
   }
 }
 
 export default App;
+
+function convertFloat32ArrayToInt16Array(float32Array: Float32Array) {
+  const result = Int16Array.from(float32Array, num => {
+    return num * 0xFFFF;
+  })
+  return result;
+}
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,27 @@
 import Koa from 'koa';
+import Http from 'http';
+import WebSocket from 'ws';
+import Speaker from 'speaker';
 import Router from './router';
 
-const app = new Koa();
+const speaker = new Speaker({
+  bitDepth: 16,
+  channels: 1,
+  sampleRate: 48000,
+});
 
+const app = new Koa();
 app.use(Router.routes());
 
-app.listen(3000);
+const server = Http.createServer();
+
+const webSocketServer = new WebSocket.Server({ server });
+webSocketServer.on('connection', ws => {
+  ws.on('message', message => {
+    speaker.write(message);
+  })
+  console.log('connected');
+});
+
+server.on('request', app.callback());
+server.listen(3000);
