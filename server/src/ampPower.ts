453a6dff008a762436ec880dc263541aa97b6620
bigfood <satcom1600@hanmail.net>
2019-03-13 14:50:44 +0900

    Fix GPIO to warn on unsupported os

--- server/src/ampPower.ts
+++ server/src/ampPower.ts
@@ -1,7 +1,17 @@
 import Onoff from 'onoff';
 
 const powerPin = 26;
-const power = new Onoff.Gpio(powerPin, 'out');
+
+function getGpio() {
+  try {
+    return new Onoff.Gpio(powerPin, 'out');
+  } catch (error) {
+    console.warn('Failed to create GPIO, It seems to be an unsupported os.');
+    return { write: () => {} };
+  }
+}
+
+const power = getGpio();
 
 function turnOn() {
   power.write(1);
